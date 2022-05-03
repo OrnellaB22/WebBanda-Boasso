3aa21312c9c940c0fff98d85793f530fee9ffda4
OrnellaB22 <ornellaboasso22@gmail.com>
2022-05-03 20:35:13 -0300

    changes
    changes

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName('toggle__button')[0]
-const navbarLinks = document.getElementsByClassName('nav__list')[0]
-
-toggleButton.addEventListener('click', () => {
-	navbarLinks.classList.toggle('active')
-})
